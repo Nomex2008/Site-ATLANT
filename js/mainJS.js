e1484b2940966c59b498ad9f042f03b0aa59e78d
Alex <97978327+Nomex2008@users.noreply.github.com>
2024-03-24 22:33:42 +0200

    Commit 23 (fix1)
    Add many fix.

--- js/mainJS.js
+++ js/mainJS.js
@@ -7,3 +7,15 @@ burger.onclick = () => {
     document.body.classList.toggle('active')
     console.log(11)
 }
+
+// Плавна прокрутка до елемента при кліці на посилання з класом header__link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
